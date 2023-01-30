6a0ffbb19155e70b1578791b2e726c59b14e21ba
Айрат <airat_vip@mail.ru>
2023-01-30 23:16:21 +0300

    Initial commit

---
+++ src/main/java/com/airat/webonlinestore/WebOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.airat.webonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airat/webonlinestore/WebOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airat.webonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
